f611348b0d396ae3b2a47e979c93dfabe28ce2c7
stephanoumenos <s_dechecchi@hotmail.com>
2019-01-21 15:54:31 -0200

    getting comfy

--- xmonad.hs
+++ xmonad.hs
@@ -3,6 +3,10 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Scratchpad
 import XMonad.Util.EZConfig
+import XMonad.Util.Run(spawnPipe)
+--import XMonad.Layout.Fullscreen
+
+import System.Exit
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
@@ -12,7 +16,7 @@ myTerminal = "xfce4-terminal --hide-menubar"
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = False
 
-myBorderWidth = 2
+myBorderWidth = 3
 
 myModMask = mod1Mask
 
@@ -32,8 +36,13 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm, xK_q), kill)
     , ((modm, xK_r), spawn "rofi -show run")
     , ((modm, xK_c), spawn "firefox")
+    , ((modm, xK_h), sendMessage Shrink)
+    , ((modm, xK_l), sendMessage Expand)
+    , ((modm,xK_m), windows W.swapMaster)
+    , ((modm, xK_comma ), sendMessage (IncMasterN 1))
+    , ((modm, xK_period), sendMessage (IncMasterN (-1)))
     , ((modm .|. shiftMask, xK_r), spawn "xmonad --recompile; xmonad --restart")
-    , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
+    , ((modm .|. shiftMask, xK_e), io (exitWith ExitSuccess))
     , ((modm .|. shiftMask, xK_F1), spawn lowerVolume)
     , ((modm .|. shiftMask, xK_F2), spawn raiseVolume)
     , ((mod4Mask, xK_t), spawn "telegram-desktop")
@@ -64,6 +73,21 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
 
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     -- Golden Ratio
+     ratio   = realToFrac $ 2/(1 + sqrt 5)
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
 myStartupHook :: X ()
 myStartupHook = do
     spawn "compton -c --backend glx --vsync opengl-swc"
@@ -72,6 +96,7 @@ myStartupHook = do
 
 myManageHooks = composeAll
     [ manageDocks
+    --, fullscreenManageHook
     ]
 
 -------------------------
@@ -93,61 +118,11 @@ defaults = def {
         mouseBindings      = myMouseBindings,
 
         manageHook = myManageHooks <+> manageHook defaultConfig,
-        layoutHook = avoidStruts  $  layoutHook defaultConfig,
+        --layoutHook = avoidStruts  $  layoutHook defaultConfig,
 
+        layoutHook         = myLayout,
         startupHook = myStartupHook
 
     }
 
 -------------------------
-
-
-help :: String
-help = unlines ["The default modifier key is 'alt'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch xterminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-p      Launch gmrun",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
-    "",
-    "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
-    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
