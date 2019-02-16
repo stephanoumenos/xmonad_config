b585ce77b5fc7285ff40d8a65c67ea59f8af05ba
stephanoumenos <s_dechecchi@hotmail.com>
2019-02-16 03:46:06 -0200

    lots of great, new stuff

--- xmonad.hs
+++ xmonad.hs
@@ -6,7 +6,8 @@ import XMonad.Util.EZConfig
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Hooks.ManageHelpers
 import XMonad.Actions.CopyWindow
---import XMonad.Layout.Fullscreen
+import XMonad.Layout.Gaps
+import XMonad.Layout.Fullscreen
 
 import System.Exit
 import System.Clipboard
@@ -19,16 +20,17 @@ import qualified Data.Map as M
 import Data.Maybe
 import Data.Ratio
 
+-- pls no bully
 myTerminal = "xfce4-terminal --hide-menubar"
 
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = False
 
-myBorderWidth = 3
+myBorderWidth = 4
 
 myModMask = mod1Mask
 
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+myWorkspaces = map show [1..10]
 
 myNormalBorderColor  = "dddddd"
 myFocusedBorderColor = "#ff69b4"
@@ -36,18 +38,17 @@ myFocusedBorderColor = "#ff69b4"
 lowerVolume =  "amixer -c 0 set Headphone 4- ; amixer -c 1 set Headphone 4- ; amixer -c 2 set Headphone 4-"
 raiseVolume =  "amixer -c 0 set Headphone 4+ ; amixer -c 1 set Headphone 4+ ; amixer -c 2 set Headphone 4+"
 
-myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
-                , NS "clementine" spawnClementine findClementine (centeredSomething 0.8 0.8)
+myScratchPads = [ NS ("terminal"++n) (myTerminal ++ " --title scratch-terminal"++n) (title =? ("scratch-terminal"++n))  manageTerm | n <- map show [1..3]]
+             ++ [ NS "clementine" "clementine" (className =? "Clementine") (proportionalCenteredSomething 0.9)
+                , NS "telegram" "telegram" (className =? "TelegramDesktop") (proportionalCenteredSomething 0.9)
+                , NS "thunderbird" "thunderbird" (className =? "Thunderbird") (proportionalCenteredSomething 0.9)
                 ]
-
-spawnTerm = myTerminal ++ " --title scratch-terminal"
-findTerm = title =? "scratch-terminal"
-
-spawnClementine = "clementine; clementine"
-findClementine = className =? "Clementine"
 
 manageSomething h w t l = customFloating $ W.RationalRect l t w h
 centeredSomething h w = manageSomething h w ((1-h)/2) ((1-h)/2)
+-- Same proportion as my monitor, only one variable to make shit easier
+-- ratio of the screen occupied
+proportionalCenteredSomething s = manageSomething s s ((1-s)/2) ((1-s)/2)
 
 manageTerm = manageSomething h w t l
     where
@@ -56,20 +57,12 @@ manageTerm = manageSomething h w t l
         t = (1-h)/2 * 1.6
         l = (1-h)/2 * 1.9
 
-
-
-
 -- I wanted mpv to open the youtube link on my clipboard and share that
 -- as a small window on all workspaces. Pretty neat!
 spawnSharedMpv = do
     possibleLink <- getClipboardString
     when (possibleLink /= Nothing) $ do
         spawn $ "mpv --title=sharedMpv " ++ fromJust possibleLink
-
-findMpv = className =? "mpv"
-
-
-manageMpv = manageTerm
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm, xK_t), spawn $ XMonad.terminal conf)
@@ -89,13 +82,18 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
     , ((modm .|. shiftMask, xK_r), spawn "xmonad --recompile; xmonad --restart")
     , ((modm .|. shiftMask, xK_e), io (exitWith ExitSuccess))
-    , ((modm .|. shiftMask, xK_F1), spawn lowerVolume)
-    , ((modm .|. shiftMask, xK_F2), spawn raiseVolume)
-    , ((mod4Mask, xK_t), spawn "telegram-desktop")
+    , ((shiftMask, xK_F1), spawn lowerVolume)
+    , ((shiftMask, xK_F2), spawn raiseVolume)
     , ((mod4Mask, xK_m), liftIO spawnSharedMpv)
     , ((mod4Mask, xK_c), windows copyToAll)
-    , ((modm, xK_F3), namedScratchpadAction myScratchPads "clementine")
-    , ((shiftMask, xK_F9), namedScratchpadAction myScratchPads "terminal")
+    , ((mod4Mask, xK_t), namedScratchpadAction myScratchPads "telegram")
+    , ((mod4Mask, xK_e), namedScratchpadAction myScratchPads "thunderbird")
+    , ((shiftMask, xK_F3), namedScratchpadAction myScratchPads "clementine")
+    , ((shiftMask, xK_F9), namedScratchpadAction myScratchPads "terminal1")
+    , ((shiftMask, xK_F10), namedScratchpadAction myScratchPads "terminal2")
+    , ((shiftMask, xK_F11), namedScratchpadAction myScratchPads "terminal3")
+    , ((0, xK_Print), spawn "scrot")
+    , ((modm,               xK_space ), sendMessage NextLayout)
     ]
 
     ++
@@ -104,9 +102,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_9] ++ [xK_0])
     , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
     ]
-
-
-
 
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
@@ -124,6 +119,11 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
 
+myGaps = [(U,18), (D,18), (L, 18), (R,18)]
+
+goldenRatio :: Rational
+goldenRatio = realToFrac $ 2/(1 + sqrt 5)
+
 myLayout = tiled ||| Mirror tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
@@ -133,8 +133,7 @@ myLayout = tiled ||| Mirror tiled ||| Full
      nmaster = 1
 
      -- Default proportion of screen occupied by master pane
-     -- Golden Ratio
-     ratio   = realToFrac $ 2/(1 + sqrt 5)
+     ratio   = goldenRatio
 
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
@@ -144,12 +143,14 @@ myStartupHook = do
     spawn "compton -c --backend glx --vsync opengl-swc"
     spawn "xset m 0 0"
     spawn "feh --bg-scale \"$(cat .config/i3/wallpaperlocation)\""
+    spawn "xsetroot -cursor_name left_ptr"
 
 myManageHooks = composeAll
                 [ manageDocks
                 --, fullscreenManageHook
                 , title =? "sharedMpv" --> doRectFloat (W.RationalRect (1 % 6) (1 % 6) (1 % 4) (1 % 4))
                 , namedScratchpadManageHook myScratchPads
+                , fullscreenManageHook
                 ]
 
 -------------------------
